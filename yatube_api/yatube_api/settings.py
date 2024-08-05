fb9ce616e1c8683ef466806a6d13062ffc23eca0
Albina Shaibakova <albinashaibakova@gmail.com>
2024-08-05 23:27:53 +0500

    initial commit

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -82,7 +82,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
